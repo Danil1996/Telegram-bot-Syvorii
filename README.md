f605a8d260cba8500cf801b040e8d70afab747ab
Данил Белинский <daniil_belinskii@MacBook-Air-Danil.local>
2020-11-05 10:07:04 +0200

    init project

---
+++ README.md
@@ -0,0 +1,40 @@
+      *TelegaByDanilBot*
+
+What is it?
+
+---
+
+TelegaByDanilBot this is the api for telegram bot.
+
+The Latest Version
+
+---
+
+This is first version of project.
+
+Documentation
+
+---
+
+Telegram-bot-api documentation : 'https://core.telegram.org/bots/api'.
+Node.js : 'https://nodejs.org/ru/download/'.
+
+Installation
+
+---
+
+Using this link 'https://github.com/Danil1996/Telegram-bot-Syvorii.git'
+you download all the files and place them in a folder. Then in the
+terminal you activate the index.js file through the node (node index.js).
+Сongratulations you can find @telegaByDanilBot in telegram and use.
+
+Software Notice
+
+---
+
+This api uses the @ botFather telegram bot for registration, as well as
+the library for creating telegram bots on Node.js.
+
+Contacts
+
+---
---
+++ src/index.js
@@ -0,0 +1,65 @@
+'use strict'
+
+const TelegramBot = require('node-telegram-bot-api'); // connect telergam-bot-api
+
+const token = '1467634524:AAHAgwZTKIziO8rSEfaOkX9FfPKj6Gwucuc'; // This is token given by telegram bot @BotFather
+
+
+const bot = new TelegramBot(token, {polling: true});
+
+//add config for keyboard  
+const keyboard = [
+    [
+      {
+        text: 'Поздороватся', 
+        callback_data: 'hello'
+      }
+    ],
+    [
+        {
+          text: 'Попращатся',
+          callback_data: 'goodBy'
+        }
+    ],
+  ];
+
+  // event handler for sending us any message
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id; 
+
+  //send a message
+  bot.sendMessage(chatId, 'Привет! Чаво хочешь?', {
+        reply_markup: {
+            inline_keyboard: keyboard
+        }
+    });
+});
+
+// keyboard press event handler
+bot.on('callback_query', (query) => {
+    const chatId = query.message.chat.id;
+
+    let img = '';
+
+    if (query.data === 'hello') { 
+        img = '../img/IMAGE 2020-11-02 10:25:30.jpg';
+    }
+
+    if (query.data === 'goodBy') { 
+        img = '../img/IMAGE 2020-11-02 10:36:42.jpg';
+    }
+
+    if (img) {
+        bot.sendPhoto(chatId, img, {
+            reply_markup: {
+                inline_keyboard: keyboard
+            }
+        });
+    } else {
+        bot.sendMessage(chatId, 'Непонятно, давай попробуем ещё раз?', { 
+            reply_markup: {
+                inline_keyboard: keyboard
+            }
+        });
+    }
+  });
